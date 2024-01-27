a2f447a97bf980f7ebfca27cb98b66d6d97f7748
maxileal <maxi.leal.2104@gmail.com>
2024-01-27 18:50:02 -0300

    avance de la logica de CRUD

--- js/admin.js
+++ js/admin.js
@@ -21,80 +21,200 @@ class Game {
     this._stockQuantity = stock;
     this._category = categoria;
     this._image = urlimg;
-    this._requirements = {
-      general: general,
-      so: sistemaOperativo,
-      processor: procesador,
-      memory: memoria,
-      graphic: grafica,
-      storage: almacenamiento,
-    };
+    this._general = general;
+    this._so = sistemaOperativo;
+    this._processor = procesador;
+    this._memory = memoria;
+    this._graphic = grafica;
+    this._storage = almacenamiento;
   }
 
   // Getters
-  get getCode() {
+  getCode() {
     return this._code;
   }
 
-  get getName() {
+  getName() {
     return this._name;
   }
 
-  get getDescription() {
+  getDescription() {
     return this._description;
   }
 
-  get getPrice() {
+  getPrice() {
     return this._price;
   }
 
-  get getStockQuantity() {
+  getStockQuantity() {
     return this._stockQuantity;
   }
 
-  get getCategory() {
+  getCategory() {
     return this._category;
   }
 
-  get getImage() {
+  getImage() {
     return this._image;
   }
 
-  get getRequirements() {
-    return this._requirements;
+  getGeneral() {
+    return this._general;
+  }
+
+  getSo() {
+    return this._so;
+  }
+
+  getProcessor() {
+    return this._processor;
+  }
+
+  getMemory() {
+    return this._memory;
+  }
+
+  getGraphic() {
+    return this._graphic;
+  }
+
+  getStorage() {
+    return this._storage;
   }
 
   // Setters
-  set setCode(value) {
+  setCode(value) {
     this._code = value;
   }
 
-  set setName(value) {
+  setName(value) {
     this._name = value;
   }
 
-  set setDescription(value) {
+  setDescription(value) {
     this._description = value;
   }
 
-  set setPrice(value) {
+  setPrice(value) {
     this._price = value;
   }
 
-  set setStockQuantity(value) {
+  setStockQuantity(value) {
     this._stockQuantity = value;
   }
 
-  set setCategory(value) {
+  setCategory(value) {
     this._category = value;
   }
 
-  set setImage(value) {
+  setImage(value) {
     this._image = value;
   }
 
-  set setRequirements(value) {
-    this._requirements = value;
+  setGeneral(value) {
+    this._general = value;
+  }
+
+  setSo(value) {
+    this._so = value;
+  }
+
+  setProcessor(value) {
+    this._processor = value;
+  }
+
+  setMemory(value) {
+    this._memory = value;
+  }
+
+  setGraphic(value) {
+    this._graphic = value;
+  }
+
+  setStorage(value) {
+    this._storage = value;
+  }
+
+  // Método para guardar un juego en el localStorage
+  saveToLocal() {
+    const existingGameData = localStorage.getItem(`${this._code}`);
+
+    if (existingGameData) {
+      console.log(
+        `La entrada con el código ${this._code} ya existe en el localStorage.`
+      );
+    } else {
+      localStorage.setItem(`${this._code}`, JSON.stringify(this));
+    }
+  }
+
+  // Método para actualizar un juego en el localStorage
+  updateLocal() {
+    let games = JSON.parse(localStorage.getItem("games")) || [];
+    const index = games.findIndex((game) => game._code === this._code);
+    if (index !== -1) {
+      games[index] = this.toJSON();
+      localStorage.setItem("games", JSON.stringify(games));
+    }
+  }
+
+  // Método para eliminar un juego del localStorage
+  deleteLocal() {
+    let games = JSON.parse(localStorage.getItem(`${this._code}`)) || {};
+    delete games[this._code];
+    localStorage.setItem("games", JSON.stringify(games));
+  }
+
+  showFromLocal() {
+    const game = localStorage.getItem(`${this._code}`);
+
+    if (game) {
+      const formattedGame = JSON.stringify(JSON.parse(game), null, 2);
+
+      console.log(
+        `Datos del juego con código ${this._code} en localStorage:\n${formattedGame}`
+      );
+    } else {
+      console.log(
+        `No se encontraron datos para el juego con código ${this._code} en localStorage.`
+      );
+    }
   }
 }
 
+// Crear una instancia de la clase Game
+const exampleGame = new Game(
+  "001",
+  "Ejemplo Game",
+  "Descripción del juego de ejemplo",
+  29.99,
+  100,
+  "Aventura",
+  "url_de_la_imagen.jpg",
+  "General info del juego",
+  "Windows 10",
+  "Intel Core i5",
+  "8GB RAM",
+  "NVIDIA GTX 1060",
+  "256GB SSD"
+);
+const exampleGame2 = new Game(
+  "002",
+  "Ejemplo Game",
+  "Descripción del juego de ejemplo",
+  29.99,
+  100,
+  "Aventura",
+  "url_de_la_imagen.jpg",
+  "General info del juego",
+  "Windows 10",
+  "Intel Core i5",
+  "8GB RAM",
+  "NVIDIA GTX 1060",
+  "256GB SSD"
+);
+
+exampleGame.saveToLocal();
+exampleGame2.saveToLocal();
+exampleGame.showFromLocal();
+exampleGame2.showFromLocal();
